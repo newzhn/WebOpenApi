48f5d15f6472e58a357be027d3de6676fdbecbb8
zhn <zhanhaonan1@163.com>
2023-06-13 22:32:14 +0800

    重构后端项目

---
+++ WebOpenApi-backend/src/main/java/com/zhn/webopenapibackend/common/ErrorCode.java
@@ -0,0 +1,45 @@
+package com.zhn.webopenapibackend.common;
+
+/**
+ * 自定义错误码
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+public enum ErrorCode {
+
+    /**
+     * 错误码
+     */
+    PARAMS_ERROR(40000, "请求参数错误"),
+    NOT_LOGIN_ERROR(40100, "用户未登录"),
+    NO_AUTH_ERROR(40101, "用户无权限"),
+    NOT_FOUND_ERROR(40400, "请求数据不存在"),
+    FORBIDDEN_ERROR(40300, "禁止访问"),
+    SYSTEM_ERROR(50000, "系统内部异常"),
+    OPERATION_ERROR(50001, "请求操作失败");
+
+    /**
+     * 状态码
+     */
+    private final int code;
+
+    /**
+     * 信息
+     */
+    private final String message;
+
+    ErrorCode(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
---
+++ WebOpenApi-backend/src/main/java/com/zhn/webopenapibackend/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.zhn.webopenapibackend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhn
+ * @version 1.0
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
